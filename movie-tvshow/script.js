25cc35e0509b51f1d0ada9ad392486e47b5724fa
Rickyocon <94875214+Rickyocon@users.noreply.github.com>
2022-05-10 17:11:05 -0400

    Add files via upload

---
+++ movie-tvshow/script.js
@@ -0,0 +1,36 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+// prints "hi" in the browser's dev tools console
+console.log("hi");
+
+var canvas;
+var h1;
+var x = 100;
+var y = 100;
+var my_fav;
+var x_speed = 0.1;
+var y_speed = -0.1;
+var bgcolor;
+var slider;
+var my_fav;
+var slider;
+var nameP;
+
+let input, button, greeting;
+
+
+
+function setup() {
+
+  
+  
+  
+}
+
+
+function draw() {
+
+
+  
+}
